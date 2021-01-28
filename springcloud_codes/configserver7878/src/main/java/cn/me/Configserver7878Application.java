dc0e6afdbc6826433458e25d635e037a20f4d09a
demerzelxd <demerzelxd@gmail.com>
2021-01-28 10:42:08 +0800

    配置中心自动刷新，webhook自动发送请求，natapp内网穿透

--- springcloud_codes/configserver7878/src/main/java/cn/me/Configserver7878Application.java
+++ springcloud_codes/configserver7878/src/main/java/cn/me/Configserver7878Application.java
@@ -2,6 +2,7 @@ package cn.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -9,6 +10,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 // 开启配置服务器支持，启动时会拉取远端git上的配置文件
 @EnableConfigServer
+// 扫描过滤器
+@ServletComponentScan(basePackages = "cn.me.filters")
 public class Configserver7878Application
 {
 	public static void main(String[] args)
